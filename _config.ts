0adb6b959d3ee06e6f9a98c0d7265177a95f9dd9
Oscar Otero <oom@oscarotero.com>
2023-10-02 01:36:01 +0200

    updates

--- _config.ts
+++ _config.ts
@@ -1,5 +1,5 @@
 import lume from "lume/mod.ts";
-import blog from "https://deno.land/x/lume_theme_simple_blog@v0.7.1/mod.ts";
+import blog from "https://deno.land/x/lume_theme_simple_blog@v0.12.1/mod.ts";
 
 const site = lume({
   location: new URL("https://oscarotero.com/design-tips/"),
